4e9868f45197aa1c97f4da04eb3f12dedf65a4b9
chematelegram <chfondia@gmail.com>
2022-06-22 15:13:09 +0200

    zippo: Delete AromaFM

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -71,6 +71,7 @@ if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
 	export FOX_USE_TWRP_RECOVERY_IMAGE_BUILDER=1
 	export LC_ALL="C"
  	export ALLOW_MISSING_DEPENDENCIES=true
+ 	export FOX_DELETE_AROMAFM=1
 
 	# OTA for custom ROMs
 	export OF_SUPPORT_ALL_BLOCK_OTA_UPDATES=1
